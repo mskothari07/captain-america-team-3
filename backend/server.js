4dffe390f1b7415fe4315194471e8529117e3dee
Meet Kothari <meet.kothari@blazeclan.com>
2022-07-17 13:01:55 +0530

    Intial Stage of Frontend and Backend

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const notes = require("./data/notes");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("Finally My First API");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+cd;
+app.get("/api/notes/:id", (req, res) => {
+  const note = notes.find((n) => n._id === req.params.id);
+  res.send(note);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server Started on Port on ${PORT}`));
